d366eefb096941985aa45eecc8b40ba2199f53fe
sleepyexe <67649181+sleepyexe@users.noreply.github.com>
2022-11-24 00:55:02 +0700

    Add files via upload

---
+++ html/language_nl.js
@@ -0,0 +1,17 @@
+var _U = {
+
+	"sign_terms" : [
+		"Dit document wordt, wanneer het is ondertekend, een officieel juridisch document.",
+        "Door dit document te ondertekenen bent u wettelijk gebonden aan de context en aanvaardt u elk juridisch gevolg dat het kan veroorzaken.",
+        "Elk exemplaar van dit document is in waarde gelijk aan het origineel. Wees extra voorzichtig bij het geven van kopieën.",
+        "Zorg ervoor dat u volledig op de hoogte bent van de inhoud van dit document voordat u het ondertekent.",
+        "Aarzel niet om hulp te zoeken bij een juridische raadgever voordat u tekent."
+	],
+	"title_information" : "INFORMATIE",
+	"title_fill_data" 	: "VUL DE VOLGENDE VELDEN IN",
+	"terms_and_signing" : "VOORWAARDEN & TEKENEN",
+	"first_name" 		: "Voornaam:",
+	"last_name" 		: "Achternaam:",
+	"birth_date" 		: "Geboortedatum:",
+	"occupation" 		: "Beroep:"
+}
